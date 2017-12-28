9373f8b326f7dfb2d12c54049b455709569cede2
David <gelpiorama@gmail.com>
2017-12-28 17:39:13 +0100

    proyecto Cotxox examen dual febrero 2017

---
+++ org/foobarspam/cotxox/carrera/Carrera.java
@@ -0,0 +1,106 @@
+package org.foobarspam.cotxox.carrera;
+
+import org.foobarspam.cotxox.conductores.Conductor;
+import org.foobarspam.cotxox.conductores.PoolConductores;
+import org.foobarspam.cotxox.tarifa.Tarifa;
+
+public class Carrera {
+
+	private String tarjetaCredito = null;
+	
+	private String origen = null;
+	private String destino = null;
+	private double distancia = 0d;
+	private int tiempoEsperado = 0;
+	private int tiempoCarrera = 0;
+	private double costeTotal = 0;
+	private int propina = 0;
+	
+	private Conductor conductor = null;
+	
+	
+	public Carrera(String tarjetaCredito){
+		this.tarjetaCredito = tarjetaCredito;
+	}
+	
+
+	public String getTarjetaCredito(){
+		return this.tarjetaCredito;
+	}
+	
+	public void setOrigen(String origen){
+		this.origen = origen;
+	}
+	
+	public String getOrigen(){
+		return this.origen;
+	}
+	
+	public void setDestino(String destino){
+		this.destino = destino;
+	}
+	
+	public String getDestino(){
+		return this.destino;
+	}
+	
+	public void setDistancia(double distancia){
+		this.distancia = distancia;
+	}
+	
+	public double getDistancia(){
+		return this.distancia;
+	}
+	
+	public double getCosteEsperado(){
+		return Tarifa.getCosteTotalEsperado(this);
+	}
+
+	public int getTiempoEsperado() {
+		return tiempoEsperado;
+	}
+
+	public void setTiempoEsperado(int tiempoEsperado) {
+		this.tiempoEsperado = tiempoEsperado;
+	}
+	
+	public int getTiempoCarrera() {
+		return tiempoCarrera;
+	}
+
+	public void setTiempoCarrera(int tiempoCarrera) {
+		this.tiempoCarrera = tiempoCarrera;
+	}
+	
+	public void setConductor(Conductor conductor){
+		this.conductor = conductor;
+	}
+	
+	public Conductor getConductor(){
+		return this.conductor;
+	}
+	
+	public void asignarConductor(PoolConductores conductores){
+		setConductor(conductores.asignarConductor());
+	}
+	
+	public void realizarPago(double pago){
+		this.costeTotal = pago;
+	}
+	
+	public double getCosteTotal(){
+		return this.costeTotal;
+	}
+	
+	public void recibirPropina(int propina){
+		this.propina = propina;
+	}
+	
+	public int getPropina(){
+		return this.propina;
+	}
+	
+	public void liberarConductor(){
+		getConductor().setOcupado(false);
+	}
+}
---
+++ org/foobarspam/cotxox/test/ConductorTest.java
@@ -0,0 +1,33 @@
+package org.foobarspam.cotxox.test;
+
+import static org.junit.Assert.*;
+
+import org.foobarspam.cotxox.conductores.Conductor;
+import org.junit.Test;
+
+public class ConductorTest {
+
+	@Test
+	public void ConstructorYgetConductorTest(){
+		String nombre = "Samantha";
+		Conductor conductor = new Conductor(nombre);
+		assertEquals(nombre, conductor.getNombre());		
+	}
+	
+	@Test
+	public void ValoracionMediaTest(){
+		String nombre = "Samantha";
+		Conductor conductor = new Conductor(nombre);
+		conductor.setValoracion((byte) 5);
+		assertEquals(1, conductor.getNumeroValoraciones(), 0);
+		assertEquals(5, conductor.getValoracion(), 0.1);
+		
+		int numeroValoraciones = 0;
+		for(int valoracion=0; valoracion<=5; valoracion++){
+			conductor.setValoracion((byte) valoracion);
+			numeroValoraciones++;
+		}
+		assertEquals(numeroValoraciones+1, conductor.getNumeroValoraciones(), 0);
+		assertEquals(2.875, conductor.getValoracion(), 0.1);
+	}
+}
