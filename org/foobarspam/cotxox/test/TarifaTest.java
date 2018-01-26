812832128ced0d3ba4d0eb41e709790aeb849304
David <gelpiorama@gmail.com>
2018-01-26 19:22:00 +0100

    formato codigo segun convencion oracle

--- org/foobarspam/cotxox/test/TarifaTest.java
+++ org/foobarspam/cotxox/test/TarifaTest.java
@@ -7,7 +7,7 @@ import org.foobarspam.cotxox.tarifa.Tarifa;
 import org.junit.Test;
 
 public class TarifaTest {
-	
+
 	Carrera carrera = new Carrera("4929637175949220");
 
 	@Test
@@ -15,34 +15,33 @@ public class TarifaTest {
 		double distancia = 7.75;
 		double esperado = 10.4625;
 		double delta = 0.01;
-		
-		assertEquals(esperado, Tarifa.getCosteDistancia(distancia), delta);		
+
+		assertEquals(esperado, Tarifa.getCosteDistancia(distancia), delta);
 	}
-	
-	
+
 	@Test
-	public void getCosteTiempo(){
+	public void getCosteTiempo() {
 		int minutos = 10;
 		double costeTiempoEsperado = 3.5;
 		double delta = 0.01;
-		assertEquals(costeTiempoEsperado, Tarifa.getCosteTiempo(minutos), delta);	
+		assertEquals(costeTiempoEsperado, Tarifa.getCosteTiempo(minutos), delta);
 	}
-	
+
 	@Test
-	public void getCosteTotalEsperado(){
+	public void getCosteTotalEsperado() {
 		carrera.setDistancia(7.75);
 		carrera.setTiempoEsperado(10);
-		double costeTotal = 7.75*1.35 + 0.35*10;
+		double costeTotal = 7.75 * 1.35 + 0.35 * 10;
 		double delta = 0.01;
-		assertEquals(costeTotal, Tarifa.getCosteTotalEsperado(carrera), delta);	
+		assertEquals(costeTotal, Tarifa.getCosteTotalEsperado(carrera), delta);
 	}
-	
+
 	@Test
 	public void getCosteTotalEsperadoMinimoTest() {
 		carrera.setDistancia(2);
 		carrera.setTiempoEsperado(2);
 		double delta = 0.01;
-		assertEquals(5, Tarifa.getCosteTotalEsperado(carrera), delta);	
+		assertEquals(5, Tarifa.getCosteTotalEsperado(carrera), delta);
 	}
 
 }
